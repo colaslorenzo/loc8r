c80f24004e690395c49431b23e1997786a39baa3
gsus colas <colaslorenzo@hotmail.com>
2016-08-04 17:23:57 -0500

    dedicado a paco y papa

---
+++ app_server/models/db.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connecton error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnnected');
+});
+var gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
